bc254cd6f90e30729b6e66fbae020b900c5cb578
thegradle <adamchuk.v.1@gmail.com>
2022-07-02 23:56:54 +0300

    Update

---
+++ app/Http/Controllers/ProfilesController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
+
+class ProfilesController extends Controller
+{
+    public function index(User $user)
+    {
+        dd($user);
+        return view('profiles.index', compact('user'));
+    }
+
+    public function edit()
+    {
+        $user = auth()->user();
+
+        return view('profiles.edit', compact('user'));
+    }
+
+    public function update(User $user)
+    {
+        //$this->authorize('update', $user->profile);
+
+        $data = request()->validate([
+            'name' => 'required',
+            'birthdate' => 'date',
+            'country' => 'required',
+            //'image' => '',
+        ]);
+
+        /*if (request('image')) {
+            $imagePath = request('image')->store('profile', 'public');
+
+            $image = Image::make(public_path("storage/{$imagePath}"))->fit(150, 150);
+            $image->save();
+
+            $imageArray = ['image' => $imagePath];
+        }*/
+
+        /*auth()->user()->profile->update(array_merge(
+            $data,
+            //$imageArray ?? []
+        ));*/
+        //auth()->user()->profile->update($data);
+
+        $user->profile->update($data);
+
+        return redirect("profile/{$user->id}");
+    }
+}
