6a15d6b9397bba4a55d46a6c628b666c87fd8c93
thegradle <adamchuk.v.1@gmail.com>
2022-07-03 21:08:57 +0300

    Update views

--- resources/views/profiles/reviews.blade.php
+++ resources/views/profiles/reviews.blade.php
@@ -24,6 +24,38 @@
             </li>
         </ul>
 
+        <h1 class="profile--title">Відгуки</h1>
 
+        <div class="profile-list">
+            <div class="profile-list-item d-flex justify-content-between">
+                <div class="profile-list-item-left d-flex justify-content-between">
+                    <img
+                        src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
+                        alt=""
+                        class="profile-list-item-left--img"
+                    >
+
+                    <div>
+                        <p class="profile-list-item-left--title">
+                            Гарний готель для відпочинку
+                        </p>
+
+                        <p class="profile-list-item-left--subtitle">
+                            Arcadia apartment & sea terrace
+                        </p>
+
+                        <p class="profile-list-item-left--subtitle">
+                            Оцінка: 9/10
+                        </p>
+                    </div>
+                </div>
+
+                <div class="profile-list-item-right d-flex align-items-start">
+                    <button class="btn profile-list-item-right--btn">
+                        <img src="/img/svg/more.svg" alt="" title="Властивості">
+                    </button>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
--- resources/views/profiles/saved.blade.php
+++ resources/views/profiles/saved.blade.php
@@ -24,6 +24,30 @@
             </li>
         </ul>
 
+        <h1 class="profile--title">Збережене</h1>
 
+        <div class="profile-list">
+            <div class="profile-list-item d-flex justify-content-between">
+                <div class="profile-list-item-left d-flex justify-content-between">
+                    <img
+                        src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
+                        alt=""
+                        class="profile-list-item-left--img"
+                    >
+
+                    <div>
+                        <p class="profile-list-item-left--title">
+                            Arcadia apartment & sea terrace
+                        </p>
+                    </div>
+                </div>
+
+                <div class="profile-list-item-right d-flex align-items-start">
+                    <button class="btn profile-list-item-right--btn">
+                        <img src="/img/svg/more.svg" alt="" title="Властивості">
+                    </button>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
